dd70bc46298852eac37ab6c3f2d395ecb36a6d69
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-05 05:31:09 +0530

    Update Commands.tsx

--- src/components/Commands.tsx
+++ src/components/Commands.tsx
@@ -1,11 +1,12 @@
 export const commands = {
   about: 'Displays information about me',
   clear: 'Clears the terminal',
-  contact: 'Displays my contact information',
+  email: 'Reach out via email',
   gui: 'Opens my portfolio website in a new tab',
   help: 'Shows this help message',
   projects: 'Shows my notable projects',
   skills: 'Lists my technical skills',
+  socials: 'Shows my social media links',
   theme: 'Changes the color theme (usage: theme <name>)',
   welcome: 'Shows the welcome message',
 };
