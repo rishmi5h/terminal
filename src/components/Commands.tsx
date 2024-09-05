64f74df02cde40cda8adff139dd576e327246bd3
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-05 05:53:30 +0530

    Update Commands.tsx

--- src/components/Commands.tsx
+++ src/components/Commands.tsx
@@ -6,7 +6,12 @@ export const commands = {
   help: 'Shows this help message',
   projects: 'Shows my notable projects',
   skills: 'Lists my technical skills',
-  socials: 'Shows my social media links',
+  socials:
+    'Shows my social media links. Use "socials <number>" to open a specific profile',
   theme: 'Changes the color theme (usage: theme <name>)',
   welcome: 'Shows the welcome message',
+};
+
+export const specialCommands = {
+  'Up Arrow': 'Go to the previous command',
 };
