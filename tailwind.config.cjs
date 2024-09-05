10f030fe0378976d7176a68cac6f1950f94e5213
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-06 03:10:17 +0530

    feat: add fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,4 +3,13 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './*.html'],
   darkMode: 'media',
   mode: 'jit',
+  theme: {
+    extend: {
+      fontFamily: {
+        consolas: ['Consolas', 'monospace'],
+        'segoe-ui': ['Segoe UI', 'sans-serif'],
+        ubuntu: ['Ubuntu Mono', 'monospace'],
+      },
+    },
+  },
 };
